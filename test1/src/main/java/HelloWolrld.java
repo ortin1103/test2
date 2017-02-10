e702aa3dcd1f50d9e444c70f89435cc39824f4a7
Strekalovskyi D <ortin@i.ua>
2017-02-10 10:58:03 +0200

    add world

--- test1/src/main/java/HelloWolrld.java
+++ test1/src/main/java/HelloWolrld.java
@@ -1,8 +1,6 @@
-/**
- * Created by denis on 10.02.17.
- */
+
 public class HelloWolrld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("World");
     }
 }
